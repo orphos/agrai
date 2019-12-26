4eef4c14e1710421c81c2baa0dd466e405a9ce93
Takezoe,Tomoaki <tomoaki3478@res.ac>
2019-12-26 20:14:41 +0900

    Run more hash functions in crypto/speed/jvm

--- crypto/speed/jvm/src/main/scala/ac/res/crypto/speed/Speed.scala
+++ crypto/speed/jvm/src/main/scala/ac/res/crypto/speed/Speed.scala
@@ -15,10 +15,22 @@ object Speed extends App {
   val buf = new Array[Byte](1024)
   r.nextBytes(buf)
   val providers = Security.getProviders
-  for (provider <- providers :+ new BouncyCastleProvider()) {
+  val hashFunctions = List(
+    "MD5",
+    "SHA-1",
+    "SHA-224",
+    "SHA-256",
+    "SHA-384",
+    "SHA-512",
+    "Skein-256-256"
+  ) ++ List("160", "256", "384", "512").map(i => s"BLAKE2B-$i") ++
+    List("224", "256", "384", "512").map(i => s"SHA3-$i")
+  for {
+    provider <- providers :+ new BouncyCastleProvider()
+    hashFunction <- hashFunctions
+  } {
     try {
-      val md = MessageDigest.getInstance("SHA-256", provider)
-      println(provider.getName)
+      val md = MessageDigest.getInstance(hashFunction, provider)
       var i = 0
       val start = System.currentTimeMillis()
       while (System.currentTimeMillis() <= start + 500) {
@@ -29,7 +41,7 @@ object Speed extends App {
       }
       val byteCount = i * 1024
       val speed = byteCount * 2 / 1024d / 1024
-      println(s"$speed MiB/s, ${i * 1024} bytes in 500 ms")
+      println(s"$hashFunction, ${provider.getName}: $speed MiB/s, ${i * 1024} bytes in 500 ms")
     } catch {
       case _: NoSuchAlgorithmException =>
     }
