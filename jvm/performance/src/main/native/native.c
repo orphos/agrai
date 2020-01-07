49193b07814a37afc0d9ba4052d2a0d38807b4b3
Takezoe,Tomoaki <tomoaki3478@res.ac>
2020-01-07 18:34:43 +0900

    Fix handling of mxcsr register

--- jvm/performance/src/main/native/native.c
+++ jvm/performance/src/main/native/native.c
@@ -3,16 +3,18 @@
 #include <math.h>
 #include <emmintrin.h>
 
+static const unsigned int mxcsr_precision_mask = 1 << 5;
+
 JNIEXPORT jdouble JNICALL Java_ac_res_jvm_benchmark_JNI_addDoubleExact(JNIEnv *jenv, jdouble left, jdouble right)
 {
 #ifdef __amd64__
-  _mm_setcsr(_mm_getcsr() ^ FE_INEXACT);
+  _mm_setcsr(_mm_getcsr() & ~mxcsr_precision_mask);
 #else
   feclearexcept(FE_ALL_EXCEPT);
 #endif
   double ret = (double)left + (double)right;
 #ifdef __amd64__
-  int inexact = _mm_getcsr() & FE_INEXACT;
+  int inexact = _mm_getcsr() & mxcsr_precision_mask;
 #else
   int inexact = fetestexcept(FE_INEXACT);
 #endif
